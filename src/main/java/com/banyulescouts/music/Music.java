c22bfba2a98904f213d711aa603b36da44c49bea
Caldan11200 <calummarshall98+github@gmail.com>
2021-08-13 12:26:22 +1000

    Initial commit

---
+++ src/main/java/com/banyulescouts/music/Music.java
@@ -0,0 +1,17 @@
+package com.banyulescouts.music;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Music extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
